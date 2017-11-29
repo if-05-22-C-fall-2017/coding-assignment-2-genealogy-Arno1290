5a67ce4ca37c02efbb5cb67ffa91afed93c390dc
Notyou <arno.bluemel@gmail.com>
2017-11-29 17:51:01 +0100

    genealogy.pl
    + a screenshot of its usage in gprolog

---
+++ genealogy.pl
@@ -0,0 +1,19 @@
+parent(adam, john).
+parent(eve, john).
+parent(eve, lisa).
+parent(john, anne).
+parent(john, pat).
+parent(pat, jacob).
+parent(carol, jacob).
+
+male(adam).
+male(eve).
+male(john).
+male(pat).
+male(jacob).
+
+female(lisa).
+female(carol).
+female(anne).
+
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
